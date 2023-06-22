40f72939213780707487768dab06f73f1412bb0b
Vinicius Trindade <119625027+viiniciustrindade@users.noreply.github.com>
2023-06-22 10:00:20 -0300

    Update Banco-de-dados-registro-internacao.sql

--- Banco-de-dados-registro-internacao.sql
+++ Banco-de-dados-registro-internacao.sql
@@ -14,9 +14,6 @@ hipoteseDiagnostica varchar(500) not null,
 medico varchar(50) not null,
 CRM varchar(20) not null,
 diagnostico varchar(500) not null,
-situacao varchar(12) not null,
+situacao varchar(20) not null,
 foreign key (codPaciente) references mvtHospCadPac
 );
-
-
-ALTER TABLE mvtHospRegInt ALTER COLUMN situacao varchar(20) not null;
